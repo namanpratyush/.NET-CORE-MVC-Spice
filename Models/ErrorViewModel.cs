da98d12453a71a7d94bf7985ae1ad5a0e28fabee
naman <np1225@nyu.edu>
2020-05-03 03:12:47 +0000

    Set up code

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpiceProj___ASP.NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
