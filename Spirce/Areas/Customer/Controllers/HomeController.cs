df9591e35769496b6474039b17864aaed41d3168
naman <np1225@nyu.edu>
2020-05-03 20:38:07 +0000

    scaffolding the project

--- Spirce/Areas/Customer/Controllers/HomeController.cs
+++ Spirce/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using Spirce.Models;
 
 namespace Spirce.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
